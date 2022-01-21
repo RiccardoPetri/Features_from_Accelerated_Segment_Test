fcf8efb176ef893dee4b708fbc9e2de10c02e236
Riccardo <80333689+RiccardoP01@users.noreply.github.com>
2022-01-21 16:25:20 +0100

    Add files via upload
    Upload

---
+++ serialProjectACA/main.cpp
@@ -0,0 +1,57 @@
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>	
+
+#include <cornerDetection.h>	//Header file of the cornerDetection implementation contains class definitions
+				//and functions, instead the implementation of the class goes into the .cpp file		
+#include <iostream>
+	
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	if(argc!=2) {
+		cerr<<"ERROR: correct syntax is ./cornerDetection imageName.jpg"<<endl;		
+		return -1;
+	}
+
+	cornerDetection corner;		
+
+	Mat rootImage, keyPointImage;			/*Mat is a structure that maintains matrix / image characteristics (number of rows and columns, data type, etc.) and a pointer to the data.
+					  		So nothing prevents us from having several instances of Mat corresponding to the same data. A Mat maintains a reference count indicating
+					  		whether data should be deallocated when a particular Mat instance is destroyed.
+							*/
+
+	rootImage = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	keyPointImage = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	
+	if(!rootImage.data) {
+		cerr<<"ERROR: data have not been loaded correctly, could not open or find image"<<endl;
+		return -1;
+	}
+
+
+	
+
+	//@@@@@@@@@@@@  Mat greyScaleImage = convertPixelToGreyScale(image);       converrebbe metterlo qui in modo da far avere la matrice a tutti gli slaves?
+
+
+	//@@@@@@@@@@@@  findKeyPoints(const Mat& image)       devo metterlo nel main e gestire i passaggi dei parametri in base al rank?
+
+	cout<<"findKeyPoints() invoked\n";
+	corner.findKeyPoints(keyPointImage);
+	
+	cout<<"showKeyPoint() invoked\n";
+	corner.showKeyPoints(rootImage);	
+
+	namedWindow("Displayed Image", CV_WINDOW_NORMAL); 	//This OpenCV function with CV_WINDOW_NORMAL allows to resize the window
+	
+	imshow("Displayed Image", rootImage);			//This OpenCV function allows to display the final result in a previously defined window
+	
+	waitKey(0);
+
+	return 0;
+}
+
+	
